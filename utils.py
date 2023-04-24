bc51928513b9dac75ce169304405a9a11f746eff
Leandro Artur Custodio <leandro.arthurcustodio@gmail.com>
2023-04-24 14:02:34 -0300

    update

--- utils.py
+++ utils.py
@@ -13,45 +13,45 @@ rcParams.update({
 
 
 class Plotter(object):
-  """An util class to display the classification results."""
+    """An util class to display the classification results."""
 
-  _PAUSE_TIME = 0.05
-  """Time for matplotlib to wait for UI event."""
+    _PAUSE_TIME = 0.05
+    """Time for matplotlib to wait for UI event."""
 
-  def __init__(self) -> None:
-    fig, self._axes = plt.subplots()
-    fig.canvas.manager.set_window_title('Audio classification')
+    def __init__(self) -> None:
+        fig, self._axes = plt.subplots()
+        fig.canvas.manager.set_window_title('Classificação de Audio')
 
-    # Stop the program when the ESC key is pressed.
-    def event_callback(event):
-      if event.key == 'escape':
-        sys.exit(0)
+        # Stop the program when the ESC key is pressed.
+        def event_callback(event):
+            if event.key == 'escape':
+                sys.exit(0)
 
-    fig.canvas.mpl_connect('key_press_event', event_callback)
+        fig.canvas.mpl_connect('key_press_event', event_callback)
 
-    plt.show(block=False)
+        plt.show(block=False)
 
-  # TODO(khanhlvg): Add type hint for result once ClassificationResult added
-  # to tflite_support.task.processor module
-  def plot(self, result) -> None:
-    """Plot the audio classification result.
+    # TODO(khanhlvg): Add type hint for result once ClassificationResult added
+    # to tflite_support.task.processor module
+    def plot(self, result) -> None:
+        """Plot the audio classification result.
 
-    Args:
-      result: Classification results returned by an audio classification
-        model.
-    """
-    # Clear the axes
-    self._axes.cla()
-    self._axes.set_title('Press ESC to exit.')
-    self._axes.set_xlim((0, 1))
+        Args:
+            result: Classification results returned by an audio classification
+            model.
+        """
+        # Clear the axes
+        self._axes.cla()
+        self._axes.set_title('O Audio é:')
+        self._axes.set_xlim((0, 1))
 
-    # Plot the results so that the most probable category comes at the top.
-    classification = result.classifications[0]
-    label_list = [
-      category.category_name for category in classification.categories
-    ]
-    score_list = [category.score for category in classification.categories]
-    self._axes.barh(label_list[::-1], score_list[::-1])
+        # Plot the results so that the most probable category comes at the top.
+        classification = result.classifications[0]
+        label_list = [
+            category.category_name for category in classification.categories
+        ]
+        score_list = [category.score for category in classification.categories]
+        self._axes.barh(label_list[::-1], score_list[::-1])
 
-    # Wait for the UI event.
-    plt.pause(self._PAUSE_TIME)
+        # Wait for the UI event.
+        plt.pause(self._PAUSE_TIME)
